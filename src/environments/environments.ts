ffc9d48b4be432dce4ee39ec56c0834978414bfd
Quillbolt <minhtvqt97@gmail.com>
2023-02-22 17:26:01 +0700

    home content

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    production: true,
+    production: false,
+    api_url: 'https://localhost:3000/api',
   
     CACHE_POIS_SECONDS: 60 * 60,
     CACHE_WAITINGTIMES_SECONDS: 60 * 60,
