286380af9105318bf8ecf639b0e48b3deb2a486b
Quillbolt <minhtvqt97@gmail.com>
2023-02-28 11:24:14 +0700

    123

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,10 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 //  Hero
-textdescription:string = 'Modern water parks may also be equipped with some type of artificial surfing or bodyboarding environment, such as a wave pool or flowrider.'
+textdescription:string = ''
 headline:string = 'A Next-Generation Thrill water Ride.'
 image:string ='https://images.unsplash.com/photo-1559956144-6358aa6ba82e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
+bg_image =""
 // content
 lefter:string='https://images.unsplash.com/photo-1566230555350-59683b1d16e0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTh8ODQ4NDMyNHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60'
 righter:string='https://images.unsplash.com/photo-1562956363-bf2f1c9de6df?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3w4NDg0MzI0fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
